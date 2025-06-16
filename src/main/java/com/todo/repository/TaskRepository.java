2353daefe07c3b66e4e211a28f3e22e15a1ebaf1
Sandeep Kumar <108261838+sndpkumarjsr@users.noreply.github.com>
2025-06-17 00:01:59 +0530

    Moodifying applications.properties, Add .env, add @EntityGraph and Add Transaction

--- src/main/java/com/todo/repository/TaskRepository.java
+++ src/main/java/com/todo/repository/TaskRepository.java
@@ -3,13 +3,19 @@ package com.todo.repository;
 import com.todo.entity.Priority;
 import com.todo.entity.Status;
 import com.todo.entity.Task;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Integer> {
 
-    public List<Task> findByStatus(Status status);
+    @EntityGraph(attributePaths = "user")
+    List<Task> findByStatus(Status status);
 
-    public List<Task> findByPriority(Priority priority);
+    @EntityGraph(attributePaths = "user")
+    List<Task> findByPriority(Priority priority);
+
+    @EntityGraph(attributePaths = "user")
+    List<Task> findAll();
 }
